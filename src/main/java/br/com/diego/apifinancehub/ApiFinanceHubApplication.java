3e34edd72373d16bc6a6c8fabb233a6fd4ef0887
Diego Guilherme <diegoguilherme752@oultook.com>
2023-07-02 21:26:47 -0300

    initial commit

---
+++ src/main/java/br/com/diego/apifinancehub/ApiFinanceHubApplication.java
@@ -0,0 +1,13 @@
+package br.com.diego.apifinancehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFinanceHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFinanceHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/diego/apifinancehub/ApiFinanceHubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diego.apifinancehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFinanceHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
